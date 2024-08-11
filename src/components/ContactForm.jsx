68b890f184dd645b2d67c155b7b75d368eb09561
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-08-10 22:00:05 -0500

    Add SendBtn component on ContactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import SendBtn from "./SendBtn";
 
 const ContactForm = () => {
   const [name, setName] = useState("");
@@ -62,12 +63,7 @@ const ContactForm = () => {
           Message
         </label>
       </div>
-      <button
-        type="submit"
-        className={`w-fit min-w-32 rounded-full border-2 border-sky-500 px-4 py-2 text-lg font-medium transition-all duration-300 hover:bg-sky-500 hover:text-slate-200 focus:bg-sky-500 focus:text-slate-200 dark:border-sky-300 dark:text-slate-300 dark:hover:bg-sky-300 dark:hover:text-slate-700 dark:focus:bg-sky-300 dark:focus:text-slate-700`}
-      >
-        Send
-      </button>
+      <SendBtn />
     </form>
   );
 };
